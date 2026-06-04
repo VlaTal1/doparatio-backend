8af98aa8dbc6f3fc65db6e29a931a80620a31649
VlaTal1 <vladiktalah@gmail.com>
2026-06-04 22:14:23 +0200

    init

---
+++ src/main/java/com/vlatal/gitracker/GitrackerApplication.java
@@ -0,0 +1,13 @@
+package com.vlatal.gitracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vlatal/gitracker/GitrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlatal.gitracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
