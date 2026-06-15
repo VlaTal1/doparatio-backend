e5ced97ba8ac6d0c52abc197200d2becf72adbf8
vladyslav <vladiktalah@gmail.com>
2026-06-15 21:30:01 +0200

    Updated application name

--- src/main/java/com/vlatal/gitracker/DoparatioApplication.java
+++ src/main/java/com/vlatal/gitracker/DoparatioApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitrackerApplication {
+public class DoparatioApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GitrackerApplication.class, args);
+        SpringApplication.run(DoparatioApplication.class, args);
     }
 
 }
